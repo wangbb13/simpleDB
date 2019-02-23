f8bce326c842c4453056d5834de9bfecd455151a
wangbb13 <18800160527@163.com>
2019-02-23 22:52:32 +0800

    part2 done.

--- simpleDB.c
+++ simpleDB.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+// Definition of some types
+
 struct InputBuffer_t {
     char *buffer;
     size_t buffer_length;
@@ -10,16 +13,98 @@ struct InputBuffer_t {
 };
 typedef struct InputBuffer_t InputBuffer;
 
+enum MetaCommandResult_t {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetaCommandResult;
+
+enum PrepareResult_t {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+};
+typedef enum PrepareResult_t PrepareResult;
+
+enum StatementType_t {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+};
+typedef enum StatementType_t StatementType;
+
+struct Statement_t {
+    StatementType type;
+};
+typedef struct Statement_t Statement;
+
+// End Definition of some types
+
+
+// Definition of functions
+
+void exit_db();
+void print_prompt();
+InputBuffer* new_input_buffer();
+ssize_t getline(char **__lineptr, size_t *n, FILE *stream);
+void read_input(InputBuffer *input_buffer);
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer);
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+void execute_statement(Statement *statement);
+
+// End Definition of functions
+
+int main(int argc, char const *argv[])
+{
+    InputBuffer *input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        // handle meta commands, e.g. .exit
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command : %s\n", input_buffer->buffer);
+                continue;
+            }
+        }
+
+        // handle SQL commands
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of %s\n", input_buffer->buffer);
+            continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
+    }
+}
+
+
+// implementation of functions
+
+void print_prompt() {
+    printf("db > ");
+}
+
+void exit_db() {
+    printf("bye.\n");
+    system("pause");
+    exit(EXIT_SUCCESS);
+}
+
 InputBuffer* new_input_buffer() {
     InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0;
     input_buffer->input_length = 0;
     return input_buffer;
-}
-
-void print_prompt() {
-    printf("db > ");
 }
 
 ssize_t getline(char **__lineptr, size_t *n, FILE *stream) {
@@ -84,19 +169,33 @@ void read_input(InputBuffer *input_buffer) {
     }
 }
 
-int main(int argc, char const *argv[])
-{
-    InputBuffer *input_buffer = new_input_buffer();
-    while (true) {
-        print_prompt();
-        read_input(input_buffer);
+MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit_db();
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
 
-        if (strcmp(input_buffer->buffer, "exit") == 0 || strcmp(input_buffer->buffer, "quit") == 0) {
-            printf("bye.\n");
-            system("pause");
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command : %s \n", input_buffer->buffer);
-        }
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+    switch (statement->type) {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
     }
 }
